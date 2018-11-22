4e2fa98e6391cf7fc4cfeed3a7e07964df54c64f
claudiuilie <claudiu.ilie0322@gmail.com>
2018-11-22 14:44:26 +0200

    aa

--- wraper.js
+++ wraper.js
@@ -4,14 +4,14 @@
 //   <fieldset class="col-lg-2">
 <legend>Sensors</legend>
 Bedroom Temp: <strong id="bedroomTempValue"><span class="badge bedroom">76</span></strong></br >
-    Livingroom Temp: <strong id="livingroomTempValue"><span class="badge livingroom">76</span></strong> </br >
-        Kitchen Temp: <strong id="kitchenTempValue"><span class="badge kitchn">76</span></strong> </br >
-            Bathroom Temp: <strong id="bathroomTempValue"><span class="badge bathroom">76</span></strong> </br > 
+Livingroom Temp: <strong id="livingroomTempValue"><span class="badge livingroom">76</span></strong> </br >
+Kitchen Temp: <strong id="kitchenTempValue"><span class="badge kitchn">76</span></strong> </br >
+Bathroom Temp: <strong id="bathroomTempValue"><span class="badge bathroom">76</span></strong> </br > 
   </fieldset >
     <fieldset class="col-lg-2">
         <legend>Relays Status</legend>
         Light Status: <strong id="created"><span class="badge lightstatus">on</span></strong> </br>
-Humidity Status: <strong id="created"><span class="badge himidity status">off</span></strong> </br > 
+        Humidity Status: <strong id="created"><span class="badge himidity status">off</span></strong></br>
   </fieldset >
 </fieldset >
 
@@ -43,3 +43,11 @@ homeassistant.appendChild(relays);
 wrapper.appendChild(homeassistant);
 
 return wrapper;
+
+var jsonData = {"tempSensors":[{"temp":20,"badge":"Bedroom Temp :","unit":"°C"},{"temp":20,"badge":"Livin Temp :","unit":"°C"},{"temp":20,"badge":"Kitchen Temp :","unit":"°C"},{"temp":20,"badge":"Bathroom Temp :","unit":"°C"}],"humiditySensors":[{"humidity":20,"badge":"Bedroom humidity :","unit":"%"},{"humidity":20,"badge":"Living humidity :","unit":"%"},{"humidity":20,"badge":"Kitchen humidity :","unit":"%"},{"humidity":20,"badge":"Bathroom humidity :","unit":"%"}],"relayStatus":{"bedroom":{"status":true,"trigger":"<a href='/?relay1on'>on<a>"}}};
+
+
+for (var i = 0; i< jsonData.tempSensors.length ; i ++){
+    console.log(jsonData.tempSensors[i].badge)
+sensors.insertAdjacentHTML('afterend', `${jsonData.tempSensors[i].badge}<strong id="bedroomTempValue"><span class="badge bedroom">${jsonData.tempSensors[i].temp}</span></strong></br >`)
+  }
